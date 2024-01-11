6ddc55aedf719bace3ad6381b94c0b3887f1248a
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-01-11 13:59:02 -0800

    add '/' to useImgPrefix

--- app/hooks/useImgPrefix.ts
+++ app/hooks/useImgPrefix.ts
@@ -1,3 +1,3 @@
-const imgPrefix = process.env.NEXT_PUBLIC_BASE_PATH || 'website-source-next';
+const imgPrefix = process.env.NEXT_PUBLIC_BASE_PATH || '/website-source-next';
 
 export { imgPrefix };
