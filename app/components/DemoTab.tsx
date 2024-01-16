e3add7bbdf0d93631db7b0d86a6726ad5807a23c
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-01-16 14:06:01 -0800

    update DemoTab for correct centering with max-w and update flow graphic

--- app/components/DemoTab.tsx
+++ app/components/DemoTab.tsx
@@ -7,15 +7,17 @@ interface DemoTabProps {
     alt: string;
 }
 
-const DemoTab:React.FC<DemoTabProps> = ({code, image, alt}) => {
-  return (
-    <div className="flex gap-5 py-4 w-full xl:w-[80vw] min-w-full h-full xl:h-[500px] grid grid-cols-1 xl:grid-cols-2 gap-0 xl:relative xl:left-[calc(50%-40vw)]">
-                                <CodeBlock code={code} showLineNumbers center/>
-                            <div className="w-full flex items-center justify-center">
-                                <FeatureImage image={image} alt={alt} height="h-[500px]" />
-                            </div>
-                        </div>
-  )
+const DemoTab: React.FC<DemoTabProps> = ({ code, image, alt }) => {
+    return (
+        // <div className="w-full flex justify-content">
+            <div className="gap-5 py-4 w-full xl:w-[80vw] max-w-screen-xl h-full xl:h-[500px] grid grid-cols-1 xl:grid-cols-2 gap-0 xl:absolute xl:left-[50%] xl:translate-x-[-50%]">
+                <CodeBlock code={code} showLineNumbers center />
+                <div className="w-full flex items-center justify-center">
+                    <FeatureImage image={image} alt={alt} height="h-[500px]" />
+                </div>
+            </div>
+        // </div>
+    )
 };
 
 export default DemoTab;
