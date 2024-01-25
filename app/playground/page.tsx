cb573f66f718bf257b2d2867d8aaecc99fc5c623
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-01-24 17:30:29 -0800

    add basic userId using uuid for playground

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import Container from '../components/Container';
 import Heading from '../components/Heading';
+import useUserId from '../hooks/useUserId';
 
 const PlaygroundPage = () => {
+  const userId = useUserId();
   return (
     <Container styles="min-h-[80vh]">
       <div className="py-20">
         <Heading title="Playground" subtitle="Coming soon!" center />
+        User id: {userId}
       </div>
     </Container>
   );
