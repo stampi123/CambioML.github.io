335fdc6d0609f7d5eb2ef26eef302f3c79bc6081
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-01-25 14:38:54 -0800

    adjust typingInterval for small words, add pauseInterval variable and set to 500ms

--- app/hooks/useTypingEffect.ts
+++ app/hooks/useTypingEffect.ts
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react';
 
-export const useTypingEffect = (words: string[], typeInterval: number = 200, deleteInterval: number = 100) => {
+export const useTypingEffect = (
+  words: string[],
+  typeInterval: number = 200,
+  deleteInterval: number = 100,
+  pauseInterval = 500
+) => {
   const [wordIndex, setWordIndex] = useState(0);
   const [text, setText] = useState('');
   const [isDeleting, setIsDeleting] = useState(false);
@@ -11,15 +16,16 @@ export const useTypingEffect = (words: string[], typeInterval: number = 200, del
       const shouldDelete = isDeleting ? 1 : -1;
       setText((current) => currentWord.substring(0, current.length - shouldDelete));
       if (!isDeleting && text === currentWord) {
-        setTimeout(() => setIsDeleting(true), 100);
+        setTimeout(() => setIsDeleting(true), pauseInterval);
       } else if (isDeleting && text === '') {
         setIsDeleting(false);
         setWordIndex((current) => (current + 1) % words.length);
       }
     }
-    const timer = setTimeout(type, isDeleting ? deleteInterval : typeInterval);
+    const thisTypeInterval = words[wordIndex].length > 4 ? typeInterval : typeInterval * 1.5;
+    const timer = setTimeout(type, isDeleting ? deleteInterval : thisTypeInterval);
     return () => clearTimeout(timer);
-  }, [wordIndex, isDeleting, text, deleteInterval, typeInterval, words]);
+  }, [wordIndex, isDeleting, text, deleteInterval, typeInterval, words, pauseInterval]);
 
   return text;
 };
