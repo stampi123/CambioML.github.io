bb1a08e2ea196c3909f4d6d0c435ffc3203e15cd
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-02-29 11:22:08 -0800

    update useWindowScroll to retain isScrolled on refresh

--- app/hooks/useWindowScroll.ts
+++ app/hooks/useWindowScroll.ts
@@ -1,15 +1,38 @@
 import { useEffect, useState } from 'react';
 
 export const useWindowScroll = (scrollY: number = 50) => {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(() => {
+    // Check if there's a stored scroll position in localStorage
+    const storedScrollPosition = localStorage.getItem('scrollPosition');
+    return storedScrollPosition ? parseInt(storedScrollPosition, 10) > scrollY : false;
+  });
+
   const listenScrollEvent = () => {
     window.scrollY > scrollY ? setIsScrolled(true) : setIsScrolled(false);
   };
+
   useEffect(() => {
+    // Listen to scroll event
     window.addEventListener('scroll', listenScrollEvent);
+
     return () => {
+      // Remove event listener
       window.removeEventListener('scroll', listenScrollEvent);
     };
   }, []);
+
+  // Save scroll position to localStorage when component unmounts
+  useEffect(() => {
+    const saveScrollPosition = () => {
+      localStorage.setItem('scrollPosition', window.scrollY.toString());
+    };
+
+    window.addEventListener('beforeunload', saveScrollPosition);
+
+    return () => {
+      window.removeEventListener('beforeunload', saveScrollPosition);
+    };
+  }, []);
+
   return isScrolled;
 };
