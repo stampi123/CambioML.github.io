b56966cec7c0a0bb22cdae4d5ed03f511dc0359f
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-02-29 16:13:41 -0800

    fix 'Window is not defined' error

--- app/hooks/useWindowScroll.ts
+++ app/hooks/useWindowScroll.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 
 export const useWindowScroll = (scrollY: number = 50) => {
-  const [isScrolled, setIsScrolled] = useState(window.scrollY > scrollY);
+  const [isScrolled, setIsScrolled] = useState(false);
   const listenScrollEvent = () => {
     window.scrollY > scrollY ? setIsScrolled(true) : setIsScrolled(false);
   };
   useEffect(() => {
+    window.scrollY > scrollY ? setIsScrolled(true) : setIsScrolled(false);
     window.addEventListener('scroll', listenScrollEvent);
     return () => {
       window.removeEventListener('scroll', listenScrollEvent);
