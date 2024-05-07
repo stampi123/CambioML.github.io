ca24468d3e473762e0c891fb092a4da58a0f2ce2
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-05-07 16:31:25 -0700

    change to grid-cols-3

--- app/components/playground/ActionContainer.tsx
+++ app/components/playground/ActionContainer.tsx
@@ -20,7 +20,7 @@ const ActionContainer = () => {
 
   return (
     <div className="w-full h-full min-h-[600px] md:h-[50vh] grid grid-rows-[50px_1fr]">
-      <div className={`w-full grid grid-cols-${Object.keys(PlaygroundTabs).length}`}>
+      <div className={`w-full grid grid-cols-3`}>
         {Object.values(PlaygroundTabs).map((tab) => (
           <PlaygroundTab key={tab} label={tab} />
         ))}
