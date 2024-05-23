bba8b22b2a6efe97a99dcb8266fd9ca455a003ff
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-05-23 10:38:05 -0700

    update extractor pricing and sec pricing

--- app/pricing-sec-api-d8015005-9f60-4cba-891d-344943e4c5e8/page.tsx
+++ app/pricing-sec-api-d8015005-9f60-4cba-891d-344943e4c5e8/page.tsx
@@ -4,7 +4,7 @@ import PricingCard from '../components/pricing/PricingCard';
 const PlaygroundPage = () => {
   return (
     <div className="pb-10 w-full h-full flex flex-col justify-center items-center">
-      <PageHero title={`Pricing`} description={'Find the right plan for you.'} short />
+      <PageHero title={`SEC API Pricing`} description={'Find the right plan for you.'} short />
       <div className="h-full w-full grid grid-cols-1 lg:grid-cols-3 gap-6 p-4 mt-10 max-w-screen-xl">
         <PricingCard
           title="Starter"
@@ -16,7 +16,6 @@ const PlaygroundPage = () => {
             '10K/10Q Financials: Available in 30min**',
             '8K/6K Financials: Available in 30min',
           ]}
-          footer="*First month money back guarantee"
           license="Commercial Use. Terms apply."
         />
         <PricingCard
@@ -41,17 +40,18 @@ const PlaygroundPage = () => {
           subtitle="For redistribution licenses, multiple API keys, priority support, or unlimited data. Please contact us."
           price="contact-us"
           period="year"
-          features={[]}
+          features={['All Starter and Business features', 'Contact us for custom features']}
           color
         />
       </div>
-      <div className="h-[100px] p-8 rounded-xl w-full">
+      <div className="mt-10 p-8 rounded-xl bg-neutral-100 w-full max-w-screen-xl h-fit">
         <ul>
+          <li>*First month money back guarantee</li>
           <li>
-            *Financials include standardized balance sheet, income statement, and cash flow statement, as well as the
+            **Financials include standardized balance sheet, income statement, and cash flow statement, as well as the
             raw financial docs.
           </li>
-          <li>**Available in 30 minutes once a statement is published on SEC.</li>
+          <li>***Available in 30 minutes once a statement is published on SEC.</li>
         </ul>
       </div>
     </div>
