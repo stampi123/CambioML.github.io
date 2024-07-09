a57ff4d5390ec63c6adaf0111b0ce1bf8bd2c94e
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-07-09 13:46:33 -0700

    update old playground page to redirect to new one

--- app/playground-3a284dca-393d-4d28-8a7a-bf202d475442/page.tsx
+++ app/playground-3a284dca-393d-4d28-8a7a-bf202d475442/page.tsx
@@ -1,8 +1,19 @@
 'use client';
-import PlaygroundPageContainer from '../components/playground/PlaygroundPageContainer';
 
-const PlaygroundPage = () => {
-  return <PlaygroundPageContainer production={true} auth0Enabled={true} />;
-};
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default PlaygroundPage;
+export default function PlaygroundOld() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/playground');
+  }, [router]);
+
+  return (
+    <div className="h-[80vh] w-full flex flex-col gap-5 items-center justify-center">
+      <h1 className="text-6xl font-semibold">{`🚛 We've moved the playground!`}</h1>
+      <h2 className="text-3xl text-neutral-500 pb-10">{`Redirecting...`}</h2>
+    </div>
+  );
+}
