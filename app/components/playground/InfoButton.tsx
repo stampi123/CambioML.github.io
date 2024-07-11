fc3cccb7dcda7195f3aa583101a251b0b77acf60
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-07-11 15:18:29 -0700

    add InfoModal content for Plain Text and Table

--- app/components/playground/InfoButton.tsx
+++ app/components/playground/InfoButton.tsx
@@ -10,37 +10,46 @@ const textStyle = 'text-lg text-neutral-700 flex flex-col gap-2';
 const h1Style = 'text-2xl font-semibold';
 const h2Style = 'text-xl font-semibold pt-4';
 
-const extractContent = (
+const plainTextContent = (
   <>
     <div className={h1Style}>Extract</div>
     <div className={textStyle}>
       <div>
-        With Uniflow, you can extract the content from your raw, unstructured data, like PDFs, TXTs, and HTML files.
+        With AnyParser, you can extract the content from your raw, unstructured data, like PDFs, TXTs, and HTML files.
       </div>
       <div>
-        {`Once you've uploaded and selected a file, run the Uniflow 'Extract' flow, which will extract the content of your file and return it in a Markdown format.`}
+        {`Once you've uploaded and selected a file, run the 'Plain Text' flow, which will extract the content of your file and return it in a Markdown format.`}
       </div>
     </div>
     <div className={h2Style}>Next Steps</div>
     <div
       className={textStyle}
-    >{`You can download the output as a text file, as well as then use the 'Transform' flow to transform your extracted data into a structured format.`}</div>
+    >{`You can download the raw Markdown as a text file, or download the JSON from any extracted tables`}</div>
   </>
 );
 
-const mapContent = (
+const tableContent = (
   <>
-    <div className={h1Style}>Map</div>
+    <div className={h1Style}>Table Extraction</div>
     <div className={textStyle}>
-      <div>With Uniflow, you can extract tables from your PDFs and extract specific keys from them.</div>
+      <div>
+        With AnyParser, you can extract tables from your files, extract specific keys from them, and map it to your
+        database schema.
+      </div>
     </div>
-    <div className={h2Style}>1. Select Tables</div>
+    <div className={h2Style}>1. Extract Tables</div>
     <div
       className={textStyle}
-    >{`Once you've selected a PDF with tables, first you need to extract those tables. After running the table extract, select the tables that you'd like to extract data from.
+    >{`First, you need to extract the tables from your file. AnyParser will extract all the tables into HTML format.
 
-    In this section, you'll also have the ability to preview the raw extracted data.`}</div>
-    <div className={h2Style}>2. Map Schema</div>
+    Once extracted, you can download the raw HTML or download all the tables into an Excel file.`}</div>
+    <div className={h2Style}>2. Select Tables to Map</div>
+    <div
+      className={textStyle}
+    >{`After running the table extract, select the tables that you'd like to extract data from.
+
+    In this section, you'll also have the ability to preview each table's html.`}</div>
+    <div className={h2Style}>3. Map Schema</div>
     <div
       className={textStyle}
     >{`Once you've exracted and selected the tables, you can then add the keys that you want to extract. If you wish to remove the mapped key, click the (X) in the box. If you wish to remove an Input Key, you can click the (X) icon in its box.
@@ -52,13 +61,13 @@ const mapContent = (
     <div className={h2Style}>Next Steps</div>
     <div
       className={textStyle}
-    >{`Once you're satisfied with the output, you can hit the Download JSON button to get a JSON copy of your keys. This will only be enabled if at least one Input Key has a non-empty Mapped Key.`}</div>
+    >{`Once you're satisfied with the output, you can hit the download the CSV or JSON. This will only be enabled if at least one Input Key has a non-empty Mapped Key.`}</div>
   </>
 );
 
 const infoContent: { [key: string]: React.ReactElement } = {
-  [PlaygroundTabs.EXTRACT]: extractContent,
-  [PlaygroundTabs.MAP]: mapContent,
+  [PlaygroundTabs.PLAIN_TEXT]: plainTextContent,
+  [PlaygroundTabs.TABLE]: tableContent,
 };
 
 const InfoButton = ({ infoType }: InfoButtonProps) => {
@@ -71,7 +80,7 @@ const InfoButton = ({ infoType }: InfoButtonProps) => {
   return (
     <button
       onClick={handleInfoClick}
-      className="w-fit h-fit hover:text-cambio-blue-0 rounded-full text-neutral-700 p-1 font-bold transition duration-300"
+      className="w-fit h-fit hover:text-cambio-red rounded-full text-neutral-700 p-1 font-bold transition duration-300"
     >
       <Question size={18} weight="bold" />
     </button>
