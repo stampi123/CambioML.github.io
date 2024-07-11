ec700f269b4c03cddd36b15aa3072274371ac570
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-07-11 15:32:18 -0700

    update infomodal and Account redirect

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,7 +4,7 @@ import AccountPageContainer from './AccountPageContainer';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const AccountPage = () => {
-  const redirectUri = 'http://localhost:3000/account';
+  const redirectUri = process.env.NEXT_PUBLIC_LOGOUT_URL_ACCOUNT || 'https://www.cambioml.com/account';
   return (
     <div>
       <Auth0Provider
