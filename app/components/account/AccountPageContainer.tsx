e73b7e062e800909c0d88574c9472e9271f54117
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-07-24 18:09:49 -0700

    add updated AccountPageContainer

--- app/components/account/AccountPageContainer.tsx
+++ app/components/account/AccountPageContainer.tsx
@@ -15,6 +15,7 @@ import getApiKeysForUser from '@/app/actions/account/getApiKeysForUser';
 import { useEffect, useState } from 'react';
 import ApiKeyRow from './ApiKeyRow';
 import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 interface LoadingComponentProps {
   icon: Icon;
@@ -22,7 +23,7 @@ interface LoadingComponentProps {
 
 const LoadingComponent = ({ icon: Icon }: LoadingComponentProps) => {
   return (
-    <div className="w-full h-full bg-neutral-100 rounded-xl animate-pulse flex items-center justify-center text-neutral-600">
+    <div className="w-full h-full min-h-[200px] bg-neutral-100 rounded-xl animate-pulse flex items-center justify-center text-neutral-600">
       <Icon size={48} />
     </div>
   );
@@ -69,12 +70,14 @@ const AccountPageContainer = () => {
   const { profile, error, loading, token } = useUserProfile();
   const { apiKeys, setApiKeys } = useAccountStore();
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const STRIPE_ENABLED: boolean = false;
 
   const handleGenerateAPIKey = async () => {
     setIsLoading(true);
     try {
-      if (!profile?.email || !token) return;
-      await getNewApiKey({ clientId: profile?.email, token: token });
+      if (!profile?.sub || !token) return;
+      await getNewApiKey({ userId: profile?.sub, token: token });
       fetchApiKeys();
     } catch {
       // Do nothing
@@ -84,9 +87,9 @@ const AccountPageContainer = () => {
   };
 
   const fetchApiKeys = async () => {
-    if (!profile?.email || !token) return;
+    if (!profile?.sub || !token) return;
     try {
-      const apiKeys = await getApiKeysForUser({ clientId: profile?.email, token: token });
+      const apiKeys = await getApiKeysForUser({ userId: profile?.sub, token: token });
       setApiKeys(apiKeys);
     } catch (error) {
       toast.error(`Error fetching API keys: ${error}`);
@@ -96,11 +99,15 @@ const AccountPageContainer = () => {
     fetchApiKeys();
   }, [profile?.sub, token]); // Add dependencies here
 
+  const handleManageSubscriptions = () => {
+    window.open('https://billing.stripe.com/p/login/test_00gcP05cxgSVfjWcMM', '_blank');
+  };
+
   return (
     <div className="pb-10 w-full h-full flex flex-col justify-center items-center">
       <PageHero title="Account" short />
-      <Container styles="h-[50vh] min-h-[1000px] py-10 w-[850px]">
-        <div className="w-full h-full flex flex-col gap-12">
+      <Container styles="min-h-[70vh] h-fit py-10 w-[850px]">
+        <div className="w-full h-full flex flex-col gap-16">
           <div>
             <Heading title="Profile" />
             <div className="w-full h-[275px] flex flex-col items-center justify-center">
@@ -132,7 +139,7 @@ const AccountPageContainer = () => {
           </div>
           <div>
             <Heading title="API Keys" />
-            <div className="w-full h-[300px] flex flex-col items-center gap-8">
+            <div className="w-full h-fit min-h-[300px] flex flex-col items-center gap-8">
               {loading && <LoadingComponent icon={Key} />}
               {!loading && error && (
                 <>
@@ -140,7 +147,7 @@ const AccountPageContainer = () => {
                 </>
               )}
               {!loading && !error && !profile && (
-                <div className="w-full h-full bg-neutral-100 rounded-xl flex items-center justify-center">
+                <div className="w-full h-full min-h-[300px] bg-neutral-100 rounded-xl flex items-center justify-center">
                   <Key size={48} />
                 </div>
               )}
@@ -157,9 +164,9 @@ const AccountPageContainer = () => {
                   </div>
                   <div className="w-full">
                     <div className="text-xl font-semibold pb-4">Your Keys</div>
-                    <div className="flex flex-col gap-2 h-[350px] overflow-auto">
+                    <div className="flex flex-col gap-2 h-[150px] overflow-auto">
                       {apiKeys.length === 0 && (
-                        <div className="w-full h-full bg-neutral-100 rounded-xl flex items-center justify-center">
+                        <div className="w-full h-full min-h-[150px] bg-neutral-100 rounded-xl flex items-center justify-center">
                           <Key size={48} />
                         </div>
                       )}
@@ -170,6 +177,25 @@ const AccountPageContainer = () => {
               )}
             </div>
           </div>
+          {STRIPE_ENABLED && (
+            <div>
+              <Heading title="Subscriptions" />
+              <div className="w-full h-[300px] flex flex-col items-center gap-8">
+                <button
+                  className="w-full h-full bg-neutral-100 rounded-xl flex items-center justify-center"
+                  onClick={() => router.push('/pricing-stripe-test-b8809880-03ce-421b-a599-1da1b94a82d5')}
+                >
+                  Subscribe to a plan
+                </button>
+                <button
+                  className="w-full h-full bg-neutral-100 rounded-xl flex items-center justify-center"
+                  onClick={handleManageSubscriptions}
+                >
+                  Manage your subscriptions
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </Container>
     </div>
