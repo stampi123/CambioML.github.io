09095b8d07caccc3f55b02ebce658f2624d883a0
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-09-20 10:53:56 -0700

    update Testimonials with logo and role

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Banners from './components/homepage/Banners';
 import Capabilities from './components/homepage/Capabilities';
-import Customers from './components/homepage/Customers';
 import Hero from './components/homepage/Hero';
 import HowItWorks from './components/homepage/HowItWorks';
 import Testimonials from './components/homepage/Testimonials';
@@ -9,7 +8,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Customers />
       <Testimonials />
       <Capabilities />
       <HowItWorks />
