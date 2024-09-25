17ddf091381796179d189bb795beb649df5f9e25
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-09-25 11:30:37 -0700

    update subtitle to h2

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -13,7 +13,7 @@ const Heading = ({ title, subtitle, center, white }: HeadingProps) => {
       <h1 className={`text-4xl ${white ? 'text-white' : 'text-neutral-800'} font-semibold whitespace-pre-line`}>
         {title}
       </h1>
-      <h3 className={`font-light ${white ? 'text-white' : 'text-neutral-500'} mt-5 text-2xl`}>{subtitle}</h3>
+      <h2 className={`font-light ${white ? 'text-white' : 'text-neutral-500'} mt-5 text-2xl`}>{subtitle}</h2>
     </div>
   );
 };
