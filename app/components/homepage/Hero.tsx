a0dda5a75db85c93c4ddb57c2282fb932a029d6e
Jojo Ortiz <ortiz.jroman@gmail.com>
2024-10-23 17:04:25 -0700

    lazy load tella video

--- app/components/homepage/Hero.tsx
+++ app/components/homepage/Hero.tsx
@@ -6,9 +6,16 @@ import Heading from '../Heading';
 import Image from 'next/image';
 import { imgPrefix } from '@/app/hooks/useImgPrefix';
 import ProductHunt from '../navbar/ProductHunt';
+import { useState } from 'react';
 
 const Hero = () => {
   const router = useRouter();
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playVideo = () => {
+    setIsPlaying(true);
+  };
+
   return (
     <div className="h-fit w-full pt-20">
       <Container styles="relative z-10 h-fit min-h-[650px] py-10 lg:py-20">
@@ -16,11 +23,24 @@ const Hero = () => {
           <div className="w-full h-[500px] py-4 lg:h-full">
             <div className="flex items-center justify-center w-full h-full py-4 lg:py-20">
               <div className="relative w-full max-w-[800px] h-auto" style={{ aspectRatio: '16/9' }}>
-                <iframe
-                  className="absolute top-0 left-0 w-full h-full border-0"
-                  src="https://www.tella.tv/video/clyqcwrvm0fde09jvg77n0rgh/embed?b=1&title=1&a=1&loop=0&t=0&muted=0&wt=1"
-                  allowFullScreen
-                />
+                {isPlaying ? (
+                  <iframe
+                    className="absolute top-0 left-0 w-full h-full border-0"
+                    src="https://www.tella.tv/video/clyqcwrvm0fde09jvg77n0rgh/embed?b=1&title=1&a=1&loop=0&autoPlay=true&t=0&muted=0&wt=1"
+                    allowFullScreen
+                  />
+                ) : (
+                  <div className="relative cursor-pointer" onClick={playVideo}>
+                    <img
+                      src={imgPrefix + '/images/homepage/tella-thumbnail.png'}
+                      alt="Video thumbnail"
+                      className="w-full h-full object-cover rounded-lg"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center">
+                      <button className="bg-red-600 bg-opacity-100 text-white rounded-xl p-4 w-[75px]">▶</button>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -33,8 +53,9 @@ const Hero = () => {
                 <Image
                   src={`${imgPrefix}/images/homepage/upperright-highlight.png`}
                   alt="Badge"
-                  fill
-                  style={{ objectFit: 'contain' }}
+                  width={144} // Specify the actual width
+                  height={96} // Specify the actual height
+                  className="object-contain"
                 />
               </div>
               <Heading title="AnyParser: Vision LLM for Document Parsing" center />
